eeda9d932afbcd61e7397df5b2cfb278add8e667
lovelifealot <peredat4ik@gmail.com>
2020-05-10 15:23:47 +0500

    Fixes module exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-import {CompoundFile} from "./CompoundFile";
-export default function compoundFile(bytes: number[]) { return CompoundFile.fromBytes(bytes)};
+export {StreamDirectoryEntry}  from "./directory/StreamDirectoryEntry"
+export {StorageDirectoryEntry}  from "./directory/StorageDirectoryEntry"
+export {DirectoryEntry}  from "./directory/DirectoryEntry"
+export {RootStorageDirectoryEntry}  from "./directory/RootStorageDirectoryEntry"
+export {CompoundFile} from "./CompoundFile"
+export * from "./utils"
+export * from "./Long"
